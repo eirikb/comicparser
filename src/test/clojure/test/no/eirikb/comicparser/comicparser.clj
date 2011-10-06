b52bf0d05dc36f7b61c776d3b08acc258896a4bb
Eirik Brandtzæg <eirikb@eirikb.no>
2011-10-06 23:34:47 +0200

    Getting pixels like a boss

--- src/test/clojure/test/no/eirikb/comicparser/comicparser.clj
+++ src/test/clojure/test/no/eirikb/comicparser/comicparser.clj
@@ -1,6 +1,27 @@
 (ns test.no.eirikb.comicparser.comicparser
   (:use clojure.test)
-  (:use no.eirikb.comicparser.comicparser))
+  (:use no.eirikb.comicparser.comicparser)
+  (:import (javax.imageio ImageIO)
+           (java.io File)))
+
+(defn get-argb [img cord]
+  (let [[x y] cord
+        clr (.getRGB img x y)]
+    [(bit-and (bit-shift-right clr 24) 0xff)
+     (bit-and (bit-shift-right clr 16) 0xff)
+     (bit-and (bit-shift-right clr 8) 0xff)
+     (bit-and clr 0xff)]))
+
+(defn get-pixels [img]
+  (map #(get-argb img %) 
+       (for [x (range (.getWidth img)) 
+             y (range (.getHeight img))] [x y])))
+
+(def pixels
+  (let [image (ImageIO/read (File. "testdata/caroling.png"))]
+    (get-pixels image)))
 
 (deftest test-parse
-         (parse "Hello"))
+         (println (count pixels)))
+
+(run-tests)
