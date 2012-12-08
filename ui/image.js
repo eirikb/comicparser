60bea94de0859617c097783e5a949bd35ba95363
Eirik Brandtzæg <eirikb@eirikb.no>
2012-12-08 12:43:10 +0000

    removed comment

--- ui/image.js
+++ ui/image.js
@@ -2,7 +2,6 @@ $(function() {
     $('.thumbnails a').click(function() {
         var $img = $(this).find('img');
         render.setImageBySrc($img.attr('src'));
-        //render.setImage($img.get(0), $img.width(), $img.height());
         tabs.show(1);
     });
 });
